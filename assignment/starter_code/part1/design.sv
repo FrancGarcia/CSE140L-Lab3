25edd430b0800a8306cbe589d22390349dda1130
Francisco Garcia <frgarcia@ucsd.edu>
2023-11-08 13:02:37 -0800

    Fixing files

--- assignment/starter_code/part1/design.sv
+++
@@ -1,3 +0,0 @@
-// Code your design here
-`include "light_package.sv"
-`include "traffic_light_controller1.sv"
--- assignment/starter_code/part1/testbench.sv
+++
@@ -1,4 +0,0 @@
-// Code your testbench here
-// or browse Examples
-`include "lab3_part1_tb.sv"
-//`include "light_package.sv"
